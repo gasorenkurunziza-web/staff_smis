a2c66bdf4c61aaf0dcbf0c12a3105d571a25e0df
isro <gasore.nkurunziza@gmail.com>
2026-07-19 01:51:36 +0200

    6th commit

--- adminpanel/src/Components/Addressinfo.jsx
+++ adminpanel/src/Components/Addressinfo.jsx
@@ -19,7 +19,7 @@ const Addressinfo = () => {
       </div>
 
       {/* Content */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-5">
+      <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-4 gap-4 p-5">
         {/* District */}
         <div className="flex flex-col">
           <label className="mb-2 text-sm font-bold text-gray-700">
