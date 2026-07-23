451e81718bcdf6647ed933c9e6aca53f471c11d4
isro <gasore.nkurunziza@gmail.com>
2026-07-23 14:09:05 +0200

    hello

--- adminpanel/src/Components/Addressinfo.jsx
+++ adminpanel/src/Components/Addressinfo.jsx
@@ -25,7 +25,7 @@ const Addressinfo = ({ employee, setEmployee }) => {
       </div>
 
       {/* Content */}
-      <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-4 gap-4 p-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-5">
         {/* District */}
         <div className="flex flex-col">
           <label className="mb-2 text-sm font-bold text-gray-700">
