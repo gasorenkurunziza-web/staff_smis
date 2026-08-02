b93a51380ee72a16babb75de409eeec46ee3d75f
isro <gasore.nkurunziza@gmail.com>
2026-08-02 21:10:00 +0200

    commited on 2nd augustin

--- adminpanel/src/Components/MiddleComponents.jsx
+++ adminpanel/src/Components/MiddleComponents.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+import { GoPlus } from "react-icons/go";
+import { employees } from "../assets/assets";
+import { CiMail } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const MiddleComponents = () => {
   return (
     <div>
       {/* middle component*/}
-      <div className="min-h-screen">
+      <div className="min-h-screen p-6">
         <div className="space-y-8">
           {/* heading section*/}
           <div>
@@ -17,16 +23,89 @@ const MiddleComponents = () => {
           </div>
           {/* search and filter*/}
           <div className="flex flex-col md:flex-row md:items-center justify-between gap-4">
-            <div className="flex flex-col sm:flex-row items-center gap-3 flex -1">
+            <div className="flex flex-col sm:flex-row items-center gap-3 flex-1">
               {/*search bar*/}
               <div className="relative w-full">
                 <input
                   type="text"
                   name=""
-                  placeholder="Search by Email,name,jobtitle,phone"
+                  className="w-full pl-4 pr-10 py-2.5 bg-white border border-yellow-500 rounded-lg focus:outline-none shadow-lg"
+                  placeholder="Search by Email,Name,Job Title,Phone number"
                 />
+                <FaSearch className="absolute text-yellow-500 w-6 h-6 right-3 top-3" />
+              </div>
+              {/*search by unit*/}
+              <div className="relative w-full">
+                <select className="w-full appearance-none pl-4 pr-10 py-2.5 bg-white border text-gray-600 border-yellow-500 rounded-lg focus:outline-none shadow-lg">
+                  <option value="">Select Unit</option>
+                  <option value="unit1">Administration</option>
+                  <option value="unit2">Health</option>
+                  <option value="unit3">Vocational</option>
+                </select>
+                <IoMdArrowDropdownCircle className="absolute h-6 w-6 right-3 top-3 text-yellow-500" />
+              </div>
+              {/* search by status */}
+              <div className="relative w-full">
+                <select className="w-full appearance-none pl-4 pr-10 py-2.5 bg-white border text-gray-600 border-yellow-500 rounded-lg focus:outline-none shadow-lg">
+                  <option value="">Select Employee Status</option>
+                  <option value="active">Active</option>
+                  <option value="inactive">Inactive</option>
+                </select>
+                <IoMdArrowDropdownCircle className="absolute h-6 w-6 right-3 top-3 text-yellow-500" />
               </div>
             </div>
+            {/*Add Employee Button*/}
+            <button className="flex items-center justify-center gap-2 px-5 py-2 bg-yellow-500 text-white font-semibold rounded-lg shadow-lg hover:bg-yellow-600 transition duration-300">
+              <GoPlus />
+              <span>Add Employee</span>
+            </button>
+          </div>
+          {/* table section for employees */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {employees.map((employee) => (
+              <div
+                key={employee.id}
+                className=" relative bg-white p-4 border border-yellow-500 rounded-xl shadow-lg flex flex-col text-center items-center gap-2"
+              >
+                <div className="absolute top-2 right-2 flex items-center gap-1">
+                  <span className="w-2 h-2 rounded-full bg-green-500"></span>
+                  <span className="text-xs text-green-500">
+                    {employee.status}
+                  </span>
+                </div>
+                {/*profile image*/}
+                <div className="mt-2 mb-4 rounded-full object-cover overflow-hidden w-24 h-24">
+                  <img
+                    src={employee.image[0]}
+                    alt={employee.name}
+                    className="w-24 h-24"
+                  />
+                </div>
+                {/* employee info*/}
+                <h2 className="text-lg text-black font-semibold">
+                  {employee.name}
+                </h2>
+
+                <p className="text-sm text-gray-400">{employee.jobtitle}</p>
+                <div className="mt-2 bg-yellow-100">
+                  <p className="text-sm text-yellow-800">{employee.unit}</p>
+                </div>
+                <div className="w-full pt-4 space-y-2 text-left border-t border-gray-200">
+                  <div className="flex items-center gap-2">
+                    <CiMail className="inline-block mr-2 text-gray-500" />
+                    <span className="text-sm text-gray-600">
+                      {employee.email}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <FaPhoneAlt className="inline-block mr-2 text-gray-500 " />
+                    <span className="text-sm text-gray-600">
+                      {employee.phone}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- adminpanel/src/assets/assets.js
+++ adminpanel/src/assets/assets.js
@@ -1,3 +1,105 @@
 import bg1 from "./bg1.png";
 import bg2 from "./bg2.png";
-export const assets = { bg1, bg2 };
+import img1 from "./musore.png";
+import img2 from "./images1.jpeg";
+import img3 from "./images2.jpeg";
+import img4 from "./images3.jpeg";
+import img5 from "./images4.jpeg";
+import img6 from "./images5.jpeg";
+import img7 from "./images6.jpeg";
+import img8 from "./images8.jpeg";
+
+export const assets = {
+  bg1,
+  bg2,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+};
+export const employees = [
+  {
+    id: 1,
+    name: "John Doe",
+    status: "Active",
+    email: "john.doe@example.com",
+    image: [img1],
+    jobtitle: "Network Engineer",
+    unit: "Health",
+    phone: "0781234545",
+  },
+  {
+    id: 2,
+    name: "John gasore",
+    phone: "0781234567",
+    status: "Inactive",
+    email: "gasore.doe@example.com",
+    image: [img2],
+    jobtitle: "Software Developer",
+    unit: "Administration",
+  },
+  {
+    id: 3,
+    status: "Active",
+    name: "John israel",
+    email: "israel.doe@example.com",
+    image: [img3],
+    jobtitle: "Data engineer",
+    unit: "Vocational",
+    phone: "0781234547",
+  },
+  {
+    id: 4,
+    name: "honeybadger Doe",
+    status: "Active",
+    email: "john.doe@example.com",
+    image: [img4],
+    jobtitle: "cyber Engineer",
+    unit: "Health",
+    phone: "0781234545",
+  },
+  {
+    id: 5,
+    name: "kasita gasore",
+    phone: "0781234567",
+    status: "Inactive",
+    email: "gasore.doe@example.com",
+    image: [img5],
+    jobtitle: "Software Engineer",
+    unit: "Administration",
+  },
+  {
+    id: 6,
+    status: "Active",
+    name: "nziza israel",
+    email: "israel.doe@example.com",
+    image: [img6],
+    jobtitle: "Data Scientist",
+    unit: "Vocational",
+    phone: "0781234547",
+  },
+  {
+    id: 7,
+    status: "Active",
+    name: "ishya israel",
+    email: "israel.doe@example.com",
+    image: [img7],
+    jobtitle: "Data Scientist",
+    unit: "Vocational",
+    phone: "0781234547",
+  },
+  {
+    id: 8,
+    status: "Active",
+    name: "impano israel",
+    email: "israel.doe@example.com",
+    image: [img8],
+    jobtitle: "Data Scientist",
+    unit: "Vocational",
+    phone: "0781234547",
+  },
+];
